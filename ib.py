4e527512058d8120e51951c6ad96fb0940cbacff
Shivam Chaudhary <shivamc021999@gmail.com>
2020-12-03 19:01:44 +0530

    Experiment code complete, connection to google sheet in progress

--- ib.py
+++ ib.py
@@ -1,13 +1,53 @@
 import os
 import sys
+import win32api
 
 import numpy as np
+import pandas as pd
 
 import psychopy.visual
 import psychopy.gui
 import psychopy.core
 import random
-from ib_stimuli import experimental_setup, movement
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+from ib_stimuli import experimental_setup, movement, stimuli_questions
+
+
+SCOPES = []
+# clientID = "646209918312-dclpaepauatium46t8tg3ap3d1s8i1j3.apps.googleusercontent.com"
+# clientSecret = "PEjWU1F-yAd5tjdgVs-t6aSn"
+# sheetId = "16C6nZTZhbpzijuF0ighzySsWBnlcuVRpXm-rT7NdeUA"
+
+
+
+response_dict = {}
+while True:
+    gui = psychopy.gui.Dlg(title="Personal Information", labelButtonOK="Confirm", labelButtonCancel="Exit")
+    gui.addField("Name: ")
+    gui.addField("Email: ")
+    gui.addField("Phone: ")
+    gui.addField("Age: ")
+    gui.addField("Gender: ", choices=["", "Male", "Female", "Others"])
+    gui.show()
+    if gui.OK:
+        if "" in gui.data:
+            win32api.MessageBox(0, "Please fill all the fields.")
+            continue
+        response_dict["Name"] = gui.data[0]
+        response_dict["Email"] = gui.data[1]
+        response_dict["Phone"] = gui.data[2]
+        response_dict["Age"] = gui.data[3]
+        response_dict["Gender"] = gui.data[4]
+    else:
+        win32api.MessageBox(0, "Please enter the required data, exiting...")
+        sys.exit("Please enter required data")
+    break
+
+print(response_dict)
 
 window = psychopy.visual.Window(
     size=[800, 600],
@@ -32,7 +72,8 @@ instructions.draw()
 window.flip()
 
 trials = 16
-
+# response_df = pd.DataFrame(columns=["Name", "Email", "Phone", "Age", "Sex", "stimuli_slow",
+#                                     "stimuli_above", "notice_additional", "shape_stimuli_res", "stimuli_color_res", "stimuli_above_res"])
 
 blankScreen = psychopy.visual.Rect(
     win=window,
@@ -42,10 +83,15 @@ blankScreen = psychopy.visual.Rect(
     fillColor=[-1] * 3,
     lineColor=[-1] * 3,
 )
+slow = random.choice([True, False])  # Record in response
+response_dict["stimuli_slow"] = slow
+
 keys = psychopy.event.waitKeys(keyList=["T", "t", "q", "Q"])
 window.flip()
+
 if "T" in keys or "t" in keys:
-    for trial_index in range(1):
+    stimuli_pos = []
+    for trial_index in range(trials):
         '''
             per trial, 
             600ms frozen start screen, 
@@ -53,16 +99,30 @@ if "T" in keys or "t" in keys:
             300ms frozen screen to facilitate ambiguous counting decisions
         '''
         print("Trial {}".format(trial_index))
-        showObject = True
-        slow = random.choice([True, False])  # Record in response
+        practice_trial = True if trial_index < 3 else False
+
         clock.reset()
         while clock.getTime() < 0.6:
             blankScreen.draw()
             window.flip()
         clock.reset()
         whiteScreen, fixation, line, stimuli_list, unexpectedStim, stimPos = experimental_setup(window)
-        pass_count = movement(stimuli_list, window, fixation, line, unexpectedStim, showObject)
-        print(pass_count)
+        if trial_index == 10 or trial_index == 14 or trial_index == 15:
+            showObject = True
+            stimuli_pos.append(stimPos)
+        else:
+            showObject = False
+        pass_count = movement(stimuli_list, window, fixation, line, unexpectedStim, showObject, slow, practice_trial)
+        if trial_index == 10 or trial_index == 14 or trial_index == 15:
+            response = stimuli_questions()
+            response["pass_count_calc"] = pass_count
+            if trial_index == 10:
+                response_dict["critical_trial"] = response
+            elif trial_index == 14:
+                response_dict["divided_att_trial"] = response
+            elif trial_index == 15:
+                response_dict["full_att_trial"] = response
+
         clock.reset()
         window.flip()
         while clock.getTime() < 0.3:
@@ -70,4 +130,5 @@ if "T" in keys or "t" in keys:
             window.flip()
         window.flip()
 
+print(response_dict)
 window.close()
--- ib_stimuli.py
+++ ib_stimuli.py
@@ -1,32 +1,65 @@
+import sys
+import math
+
+import numpy as np
+import random
+import win32api
+
 import psychopy.visual
 import psychopy.gui
 import psychopy.core
-import random
-import sys
-import math
-import numpy as np
 
 stimuli_count = 8
 clock = psychopy.core.Clock()
 
 
-def movement(stimuli_list, window, fixation, line, unexpectedStim, showObject=False, slow=False):
+def stimuli_questions():
+    response = {}
+    while True:
+        gui = psychopy.gui.Dlg(title="Personal Information", labelButtonOK="Submit", labelButtonCancel="Cancel")
+        gui.addField("How many times did the red object touch/cross the middle line?")
+        gui.addField("Did you notice anything other than the 8 letter?", choices=["", "Yes", "No"])
+        gui.addText("Answer the below regardless of your previous answer.")
+        gui.addField("Was the additional object above or below the line?", choices=["", "Above", "Below"])
+        gui.addField("What was the shape of the object?", choices=["", "X", "O", "T", "L", "W"])
+        gui.addField("What was the color of the object?", choices=["", "Red", "Green", "Blue", "Grey", "Black", "Cyan"])
+        gui.show()
+        if gui.OK:
+            if "" in gui.data:
+                win32api.MessageBox(0, "Please enter the required data.")
+                continue
+            response["pass_count_resp"] = gui.data[0]
+            response["stimuli_seen"] = gui.data[1]
+            response["stimuli_position"] = gui.data[2]
+            response["stimuli_shape"] = gui.data[3]
+            response["stimuli_color"] = gui.data[4]
+        else:
+            win32api.MessageBox(0, "Please enter the required data, exiting...")
+            sys.exit("Please enter required data")
+        break
+    return response
+
+
+def movement(stimuli_list, window, fixation, line, unexpectedStim, showObject=False, slow=False, trial=True):
     clock.reset()
     i = 0
-    speed_of_stimuli = 3
+    stimuli_trial_speed = 2.25
+    stimuli_expt_speed = 4.5
+    speed_of_stimuli = stimuli_trial_speed if trial else stimuli_expt_speed
+
     low_speed_of_object = 2.25
     high_speed_of_object = 4.5
     object_speed = low_speed_of_object if slow else high_speed_of_object
+
     pass_count = 0
 
     while clock.getTime() < 8:
-        if showObject:
+        if showObject and clock.getTime() > 2.9:
             unexpectedStim.pos = [unexpectedStim.pos[0] - object_speed, unexpectedStim.pos[1]]
             unexpectedStim.draw()
         for textStim in stimuli_list:
             stim = textStim["stim"]
-            if 1.5 >= stim.pos[1] >= -1.5:  # and stim.color == "#0000FF":
-                print("pass")
+            if 1.5 >= stim.pos[1] >= -1.5 and stim.color == "#0000FF":
                 pass_count += 1
             if stim.pos[0] >= 390:
                 textStim["x_dir"] *= -1
@@ -37,8 +70,8 @@ def movement(stimuli_list, window, fixation, line, unexpectedStim, showObject=Fa
             elif stim.pos[1] <= -290:
                 textStim["y_dir"] *= -1
 
+            # change the position of the stimuli
             stim.pos = [stim.pos[0] + speed_of_stimuli * textStim["x_dir"], stim.pos[1] + speed_of_stimuli * textStim["y_dir"]]
-            stim.size = 2
             stim.draw()
             fixation.draw()
             line.draw()
